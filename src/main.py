a5b382f65c98d0987187867f85e9c6f53aea4392
Victor Luis Barbosa Nascimento <victor.luis2003@hotmail.com>
2025-11-11 12:32:34 -0300

    adjust minimal fastapi app and dockerfile

--- src/main.py
+++ src/main.py
@@ -1,10 +1,22 @@
+# from fastapi import FastAPI
+# from entrypoints.events.github_webhook import router as github_webhook_router
+
+# app = FastAPI()
+
+# @app.get("/")
+# def root():
+#     return {"message": "AI Agents backend is running!"}
+
+# app.include_router(github_webhook_router)
 from fastapi import FastAPI
-from entrypoints.events.github_webhook import router as github_webhook_router
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
-def root():
-    return {"message": "AI Agents backend is running!"}
+def home():
+    return {"message": "API funcionando!"}
 
-app.include_router(github_webhook_router)
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
